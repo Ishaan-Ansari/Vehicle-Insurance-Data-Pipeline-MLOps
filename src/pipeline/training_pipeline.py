8758cde85ddeb8011df22aab6ee50cd55ea9af0a
ishaan-ansari <ishaanansari044@gmail.com>
2026-02-07 22:12:58 +0530

    updated the training pipiline for model eval

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -6,21 +6,21 @@ from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
-# from src.components.model_evaluation import ModelEvaluation
+from src.components.model_evaluation import ModelEvaluation
 # from src.components.model_pusher import ModelPusher
 
 from src.entity.config_entity import (DataIngestionConfig,
                                         DataValidationConfig,
                                           DataTransformationConfig,
-                                          ModelTrainerConfig,)
-                                        #   ModelEvaluationConfig,
+                                          ModelTrainerConfig,
+                                          ModelEvaluationConfig,)
                                         #   ModelPusherConfig)
                                           
 from src.entity.artifact_entity import (DataIngestionArtifact,
                                         DataValidationArtifact,
                                             DataTransformationArtifact,
-                                            ModelTrainerArtifact,)
-                                            # ModelEvaluationArtifact,
+                                            ModelTrainerArtifact,
+                                            ModelEvaluationArtifact,)
                                             # ModelPusherArtifact)
 
 
@@ -31,7 +31,7 @@ class TrainPipeline:
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
-        # self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
         # self.model_pusher_config = ModelPusherConfig()
 
 
@@ -98,19 +98,19 @@ class TrainPipeline:
         except Exception as e:
             raise CustomException(e, sys)
 
-    # def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
-    #                            model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting modle evaluation
-    #     """
-    #     try:
-    #         model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
-    #                                            data_ingestion_artifact=data_ingestion_artifact,
-    #                                            model_trainer_artifact=model_trainer_artifact)
-    #         model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
-    #         return model_evaluation_artifact
-    #     except Exception as e:
-    #         raise CustomException(e, sys)
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting modle evaluation
+        """
+        try:
+            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
+                                               data_ingestion_artifact=data_ingestion_artifact,
+                                               model_trainer_artifact=model_trainer_artifact)
+            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
+            return model_evaluation_artifact
+        except Exception as e:
+            raise CustomException(e, sys)
 
     # def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
     #     """
